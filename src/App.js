420611b858b31c44214bda496ee546b8b53c75a2
Amaan Jambura <amaanjambura786@gmail.com>
2023-09-22 15:09:23 +0530

    Updated contents of the file

--- src/App.js
+++ src/App.js
@@ -12,17 +12,19 @@ import { AuthContext } from "./context/AuthContext";
 
 function App() {
   const { darkMode } = useContext(DarkModeContext);
+
   const {currentUser} = useContext(AuthContext)
+
   const RequireAuth = ({ children }) => {
     return currentUser ? children : <Navigate to="/login" />;
   };
-  // console.log(currentUser);
+
   return (
     <div className={darkMode ? "app dark" : "app"}>
       <BrowserRouter>
         <Routes>
           <Route path="/">
-          <Route path="login" element={<Login />} />
+            <Route path="login" element={<Login />} />
             <Route
               index
               element={
