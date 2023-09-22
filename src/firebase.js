fdb49e8d767fef4f508e39475b77543ee4d5d151
Amaan Jambura <amaanjambura786@gmail.com>
2023-09-22 15:09:02 +0530

    Added and Completed Insertion of User opertation with storing Profile images feature

--- src/firebase.js
+++ src/firebase.js
@@ -1,12 +1,8 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
 import { getAuth } from "firebase/auth";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyCfUku5aGnCslJq-YBa6obwepckhlWZuWQ",
   authDomain: "react-auth-crud-b28f5.firebaseapp.com",
@@ -19,5 +15,6 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+export const db = getFirestore(app);
 export const auth = getAuth();
+export const storage = getStorage(app);
